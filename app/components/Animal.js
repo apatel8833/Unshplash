0ee5d1b2e7e7a5fe60570cc225fbe16e89c6cd0b
ATUL PATEL <patul8833@gmail.com>
2023-09-22 16:06:12 +0530

    first commit

--- app/components/Animal.js
+++ app/components/Animal.js
@@ -1,7 +1,7 @@
 
 "use client"
 import axios from 'axios';
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -12,30 +12,31 @@ function Animal(props) {
 
     useEffect(() => {
         try {
-            
+
             async function fetchData() {
                 const { data } = await axios.get(`https://api.unsplash.com/search/photos?client_id=Xo1JuuXtZeFM3ke9eGQ1EoI7AV8PwnNUVE_WaVPmyqI&query=animals&page=${load}`)
                 console.log(data);
                 setImages((prev) => [...prev, ...data.results]);
-    
+
             }
             fetchData();
         } catch (error) {
-           console.log(error);         
+            console.log(error);
         }
     }, [load]);
 
-    
+
 
 
     return (
         <div>
-              <div className='poster'>
+            <div className='poster'>
                 <Image
-                src="https://source.unsplash.com/600x900/?animal"
-                className='img'
-                height={100}
-                width={100}
+                    src="https://source.unsplash.com/600x900/?animal"
+                    className='img'
+                    height={100}
+                    width={100}
+                    alt='image'
                 >
 
                 </Image>
@@ -44,7 +45,7 @@ function Animal(props) {
                     <h1>Animals</h1>
                     <p>Exotic wildlife, pet kittens — and everything in between. Uncover the beauty of the animal kingdom through your screen...</p>
                     <p>Powered by creators everywhere.</p>
-                    
+
                     <small className='pt-2'>Trending : flower, wallpapers, backgrounds, happy, love</small>
 
                 </div>
@@ -60,13 +61,15 @@ function Animal(props) {
                                 </div>
                                 <div className='item'>
                                     <Image
-                                    src={elm.user.profile_image.small}
-                                    height={100}
-                                    width={100}
-                                    className="img"
+                                        src={elm.user.profile_image.small}
+                                        height={100}
+                                        width={100}
+                                        className="img"
+                                        alt='image'
+
                                     >
                                     </Image>
-                                   
+
                                     <span>@{elm.user.username}</span>
                                     <Link href={elm.urls.small_s3}>
                                         <i className="ri-arrow-down-line"></i>
@@ -74,21 +77,23 @@ function Animal(props) {
 
                                 </div>
                                 <Image
-                                src={elm.urls.small}
-                                height={0}
-                                width={1000}
-                                className='img'
+                                    src={elm.urls.small}
+                                    height={0}
+                                    width={1000}
+                                    className='img'
+                                    alt='image'
+
                                 ></Image>
-            
+
                             </Link>
                         </>
-            )
+                    )
 
                 })}
-        </div>
-                <button onClick={()=>setLoad(load+1)} className= 'w-75 btn btn-primary '>Click here for more images..</button>
+            </div>
+            <button onClick={() => setLoad(load + 1)} className='w-75 btn btn-primary '>Click here for more images..</button>
         </div >
-        
+
     );
 }
 
